a15d2cb7c29cbf49168b8eeb5cec1b199bdf943a
Atharva Deshmukh <atharva1525@gmail.com>
2021-12-02 23:57:32 +0530

    print list using functional programming

--- src/fp01_Print_All_numbers_in_list/FP01_Structure.java
+++ src/fp01_Print_All_numbers_in_list/FP01_Structure.java
@@ -4,16 +4,24 @@ import java.util.List;
 
 public class FP01_Structure {
 
-//	print All Numbers In the List in normal way
+//	print All Numbers In the List in functional programming way
 	public static void main(String[] args) {
-
-		printAllNumbersInList(List.of(12, 9, 12, 7, 8, 14, 8, 1, 2, 3));
+		System.out.println("Functional Programming: ");
+		printAllNumbersInListFunctional(List.of(12, 9, 12, 7, 8, 14, 8, 1, 2, 3));
+	}
+	
+	private static void print(int num) {
+		System.out.println(num);
 	}
 
-	private static void printAllNumbersInList(List<Integer> numbers) {
-		for (Integer integer : numbers) {
-			System.out.println(integer);
-		}	
+	private static void printAllNumbersInListFunctional(List<Integer> numbers) {
+		
+//		What to do?
+//		Convert to stream
+		
+		numbers.stream().forEach(FP01_Structure::print);
+
+//		numbers.stream().forEach(FP01_Structure::print);
 	}
 
 }
